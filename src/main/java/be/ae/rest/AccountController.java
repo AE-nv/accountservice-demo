0e19568cd6cc23ae8062ff8f42b09c857ea76131
Thomas Petit <thomas.marc.petit@gmail.com>
2016-09-27 16:49:41 +0200

    removed any implementation only rest

--- src/main/java/be/ae/rest/AccountController.java
+++ src/main/java/be/ae/rest/AccountController.java
@@ -2,10 +2,6 @@ package be.ae.rest;
 
 import be.ae.rest.model.AccountResource;
 import be.ae.rest.model.CreateAccountCommand;
-import be.ae.services.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityLinks;
-import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,38 +10,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.net.URISyntaxException;
 import java.util.List;
 
 @Controller
 @RequestMapping("/accounts")
-@ExposesResourceFor(AccountResource.class)
 public class AccountController {
-
-    @Autowired
-    private AccountService accountService;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AccountResource>> list() {
-        final List<AccountResource> accounts = accountService.getAccounts();
-        return ResponseEntity.ok(accounts);
+        //TODO
+        return ResponseEntity.ok(null);
     }
 
     @RequestMapping(value = "/{id}")
     public ResponseEntity<AccountResource> get(@PathVariable String id) {
-        final AccountResource accountResource = accountService.get(id);
-        return ResponseEntity.ok(accountResource);
+        //TODO
+        return ResponseEntity.ok(null);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> create(@RequestBody CreateAccountCommand createAccountCommand) {
-        final String id = accountService.create(createAccountCommand);
-        return ResponseEntity.noContent().build();
+        //TODO
+        return ResponseEntity.created(null).build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable String id) {
-        accountService.delete(id);
+        //TODO
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/be/ae/services/model/AccountType.java
+++
@@ -1,5 +0,0 @@
-package be.ae.services.model;
-
-public enum AccountType {
-    CHECKING, SAVINGS;
-}
