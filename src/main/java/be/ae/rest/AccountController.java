9d8c8a836d1995f5008d930c337859cff61c3c27
Thomas Petit <thomas.marc.petit@gmail.com>
2016-09-27 17:03:30 +0200

    removed import rules

--- src/main/java/be/ae/rest/AccountController.java
+++ src/main/java/be/ae/rest/AccountController.java
@@ -1,16 +1,10 @@
 package be.ae.rest;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/accounts")
 public class AccountController {
-    
+
 }
