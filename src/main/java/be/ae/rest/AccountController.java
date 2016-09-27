99346f0d5cd4cb2b78414d1f4e8e68a3da0052eb
Thomas Petit <thomas.marc.petit@gmail.com>
2016-09-27 17:00:37 +0200

    remove impl

--- src/main/java/be/ae/rest/AccountController.java
+++ src/main/java/be/ae/rest/AccountController.java
@@ -1,12 +1,9 @@
 package be.ae.rest;
 
-import be.ae.rest.model.AccountResource;
-import be.ae.rest.model.CreateAccountCommand;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,19 +14,19 @@ import java.util.List;
 public class AccountController {
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<AccountResource>> list() {
+    public ResponseEntity<List<String>> list() {
         //TODO
         return ResponseEntity.ok(null);
     }
 
     @RequestMapping(value = "/{id}")
-    public ResponseEntity<AccountResource> get(@PathVariable String id) {
+    public ResponseEntity<String> get(@PathVariable String id) {
         //TODO
         return ResponseEntity.ok(null);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> create(@RequestBody CreateAccountCommand createAccountCommand) {
+    public ResponseEntity<?> create(String body) {
         //TODO
         return ResponseEntity.created(null).build();
     }
