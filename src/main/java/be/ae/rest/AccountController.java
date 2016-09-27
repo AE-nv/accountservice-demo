ae8902e64c06a6610f6dd4248d9f1b397bd7d4d2
Thomas Petit <thomas.marc.petit@gmail.com>
2016-09-27 17:02:11 +0200

    removed code

--- src/main/java/be/ae/rest/AccountController.java
+++ src/main/java/be/ae/rest/AccountController.java
@@ -12,28 +12,5 @@ import java.util.List;
 @Controller
 @RequestMapping("/accounts")
 public class AccountController {
-
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<String>> list() {
-        //TODO
-        return ResponseEntity.ok(null);
-    }
-
-    @RequestMapping(value = "/{id}")
-    public ResponseEntity<String> get(@PathVariable String id) {
-        //TODO
-        return ResponseEntity.ok(null);
-    }
-
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> create(String body) {
-        //TODO
-        return ResponseEntity.created(null).build();
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> delete(@PathVariable String id) {
-        //TODO
-        return ResponseEntity.noContent().build();
-    }
+    
 }
